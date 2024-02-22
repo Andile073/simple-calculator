ba06c1a43945debecb74881694be332120106a6b
Andile073 <160361993+Andile073@users.noreply.github.com>
2024-02-22 21:31:28 +0200

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        char operator;
+
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Choose operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Undefined: Cannot divide by 0!");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+    }
+}
